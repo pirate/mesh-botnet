f4a261bc3e611d8438236932c929946307f24b31
Nick Sweeting <nikisweeting@gmail.com>
2013-06-23 16:31:33 +0800

    added new function to scan entire skype db for cc numbers

--- modules/cardcheck.py
+++ modules/cardcheck.py
@@ -1,3 +1,7 @@
+import sqlite3
+import re
+from time import sleep
+
 def cardLuhnChecksumIsValid(card_number):
     """ checks to make sure that a number passes a luhn mod-10 credit card checksum """
     card_number = ''.join(card_number.split())  # remove all whitespace
@@ -13,21 +17,47 @@ def cardLuhnChecksumIsValid(card_number):
         sum = sum + digit
     return ( (sum % 10) == 0 )
 
-def printit(in1, in2):
-    print("[+] %s :%s" % (in1.ljust(26), in2))
+def ccnSniffer(messages):
+        numbers = []
+        for index, line in enumerate(messages):
+            line = line.replace("-", "").replace(" ", "")
+            while re.search(r'\d+', line) != None: 
+                number = re.search(r'\d+', line).group()
+                numbers.append((number, index))
+                line = line[line.find(number)+len(number):] # removes the first matched number from the line and searches for another match
 
-numbers = [ "378282246310005",
-            "4111 1111 1111 1111",
-            "5500 0000 0000 0004", 
-            "3400 0000 0000 009", 
-            "3000 0000 0000 04", 
-            "3000 0000 0000 04",
-            "6011 0000 0000 0004", 
-            "2014 0000 0000 009", 
-            "3088 0000 0000 0009",
-            "4222222222222",
-            "4012888888881881",]
+        interesting_numbers = []
+        last_number = 0
+        for number in numbers:
+            if 17 > len(number[0]) > 12 and number[0] != last_number:
+                interesting_numbers.append((number[0], number[1]))
+                last_number = number[0]
 
-print("[*] -- Starting Credit Card Check -- ")
-for number in numbers:
-    printit(''.join(number.split()), cardLuhnChecksumIsValid(number))
+        for number in interesting_numbers:
+            if cardLuhnChecksumIsValid(number[0]) and int(number[0]) > 10:
+                yield("[+] CC: %s" % number[0])
+                yield("[>] Context:")
+                yield("[-2]    %s" % messages[number[1]-2].replace('\n',''))
+                yield("[-1]    %s" % messages[number[1]-1].replace('\n',''))
+                yield("[ 0]    %s" % messages[number[1]].replace('\n',''))
+                try:
+                    yield("[+1]    %s" % messages[number[1]+1].replace('\n',''))
+                    yield("[+2]    %s\n" % messages[number[1]+2].replace('\n',''))
+                except:
+                    yield("[+X] Most recent message\n")
+
+if __name__ == "__main__":
+    sample_cc_numbers = [   "378282246310005",
+                            "4111 1111 1111 1111",
+                            "5500 0000 0000 0004", 
+                            "3400 0000 0000 009", 
+                            "3000 0000 0000 04", 
+                            "3000 0000 0000 04",
+                            "6011 0000 0000 0004", 
+                            "2014 0000 0000 009", 
+                            "3088 0000 0000 0009",
+                            "4222222222222",
+                            "4012888888881881",]
+
+    for line in ccnSniffer(skypeMessageParser(['/var/root/Library/Application Support/Skype/nikisweeting/main.db'])):
+        print line
